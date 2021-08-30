81e7da1edfc698513010c50913b7cc25970ad0c2
Lindrit Krasniqi <linndritks@gmail.com>
2021-08-30 09:50:31 +0200

    Update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,33 @@
 <template>
-  <Tutorial />
+  <b-row v-if="$auth.loggedIn" class="justify-content-center">
+    <b-col cols="12" md="8">
+      <b-card class="shadow rounded-lg" header="Dashboard">
+        <b-card-body class="lead">
+          Welcome, {{ $auth.user.name }}!
+        </b-card-body>
+      </b-card>
+    </b-col>
+  </b-row>
+
+  <b-col v-else cols="12" lg="6">
+    <b-card class="shadow rounded-lg">
+      <NuxtLogo />
+      <br>
+      <FormsLogin />
+    </b-card>
+  </b-col>
 </template>
 
 <script>
-export default {}
+export default {
+  layout: 'loggedIn',
+
+  auth: false,
+
+  head () {
+    return {
+      title: this.$auth.loggedIn ? 'Welcome' : 'NuxtJS with BootstrapVue'
+    }
+  }
+}
 </script>
