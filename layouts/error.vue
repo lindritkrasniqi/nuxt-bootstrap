5de72796f38a5fd331a59a9c48b6c1267064c67b
Lindrit Krasniqi <linndritks@gmail.com>
2021-08-30 09:46:53 +0200

    Create an error layout to show error pages

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+/* eslint-disable vue/require-prop-types */
+<template>
+  <b-container>
+    <b-row
+      style="height: 100vh"
+      class="d-flex justify-content-center align-content-center"
+    >
+      <b-col class="text-center">
+        <h3>{{ error.message }} | {{ error.statusCode }}</h3>
+        <nuxt-link :to="{ name: 'index' }" class="text-decoration-none">
+          Go back to home
+        </nuxt-link>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default () { return { } }
+    }
+  },
+
+  head () {
+    return {
+      title: this.error.message
+    }
+  }
+}
+</script>
