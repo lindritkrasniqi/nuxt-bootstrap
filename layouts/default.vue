fd807aa7be48071ab1b8d3dc4d3454ee99782dc3
Lindrit Krasniqi <linndritks@gmail.com>
2021-08-30 09:46:15 +0200

    Create a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container
+    id="app"
+    style="height: 100vh"
+    class="d-flex justify-content-center align-items-center"
+  >
+    <Nuxt />
+  </b-container>
+</template>
+
+<style>
+body {
+  background-color: rgb(236, 236, 236);
+}
+</style>
