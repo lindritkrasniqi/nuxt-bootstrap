8ba6c9226d1d9d3e8d39ee0dbd4e78cd4a05b15e
Lindrit Krasniqi <linndritks@gmail.com>
2021-08-30 09:35:42 +0200

    Create errors module

---
+++ store/errors.js
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+export const state = () => ({
+  message: null,
+  error: []
+})
+
+export const mutations = {
+  error: (state, data) => {
+    state.message = data.message ?? null;
+    state.error = data.errors ?? [];
+  }
+}
